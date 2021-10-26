51c2c26dc73453879a13f032e56b2e4ab8c64c04
Binil Eldho Roy <bi2277009@gmail.com>
2021-10-26 17:32:21 +0530

    Finished Model

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -127,6 +127,10 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [ BASE_DIR/ 'static' ]
 
+MEDIA_ROOT =  BASE_DIR / 'static/images'
+
+MEDIA_URL = '/images/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
